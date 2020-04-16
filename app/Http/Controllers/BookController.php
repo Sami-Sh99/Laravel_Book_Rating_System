39f64a936cfeb3f38883de05f720d1db0561eccf
Sami-Sh99 <sami_sh.99@outlook.com>
2020-04-16 05:30:32 +0300

    Minor refactor

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,7 +14,7 @@ class BookController extends Controller
 {
     public function __construct()
     {
-     $this->middleware('auth')->except(['show']);
+     $this->middleware('auth')->except(['index','show']);
     }
 
       /**
